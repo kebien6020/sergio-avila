c9031842bbff5e33281a4ea4bf5182fd47d5dfd6
Kevin Peña <kevin.pena.prog@gmail.com>
2018-09-27 09:16:52 -0500

    Navbar initial implementation

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,27 @@
  */
 
 require('./bootstrap');
+
+
+const searchElems = document.querySelectorAll('.nav-wrapper li.search')
+for (const $elem of searchElems) {
+  $elem.addEventListener('click', function(event) {
+    const hasFocus = $elem => $elem === document.activeElement
+    const isCloseIcon = event.target.classList.contains('material-icons') &&
+                        event.target.textContent === 'close' &&
+                        hasFocus(this.querySelector('input[type="search"]'))
+    if (isCloseIcon) return
+
+    this.classList.add('show')
+    const $search = this.querySelector('input[type="search"]')
+    if ($search) $search.focus()
+
+    // Not using add event listener for easy removal of
+    // the listener
+    $search.onblur = () => {
+      // did not lose previous this because of arrow function
+      this.classList.remove('show')
+      $search.onblur = null
+    }
+  })
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        {{-- CSRF Token --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <title>Sergio Avila</title>
 
         {{-- Fonts --}}
@@ -12,15 +15,55 @@
 
         {{-- Styles --}}
         {{-- Main styles --}}
+        <link type="text/css" rel="stylesheet" href="css/vendor.css"  media="screen,projection"/>
         <link type="text/css" rel="stylesheet" href="css/app.css"  media="screen,projection"/>
 
     </head>
     <body>
-        
+      <header>
+        <nav aria-role="navigation">
+          <div class="nav-wrapper grey darken-4">
+            <a href="#" class="brand-logo center">
+              <img src="/images/logo.png" alt="Logo de la Empresa">
+            </a>
+            <a href="#" data-target="sidenav-mobile" class="sidenav-trigger">
+              <i class="material-icons">menu</i>
+            </a>
+            <ul id="nav-mobile" class="right">
+              <li><a href="#">Catálogo</a></li>
+              <li><a href="#">Categorías</a></li>
+              <li class="search">
+                <a href="#">
+                  <form>
+                    <div class="input-field">
+                      <input id="search" type="search">
+                      <label class="label-icon" for="search">
+                        <i class="material-icons">search</i>
+                      </label>
+                      <i class="material-icons">close</i>
+                    </div>
+                  </form>
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        {{-- Scripts --}}
-        <script src="/js/manifest.js" type="text/javascript" charset="utf-8"></script>
-        <script src="/js/vendor.js" type="text/javascript" charset="utf-8"></script>
-        <script src="/js/app.js" type="text/javascript" charset="utf-8"></script>
+          <ul class="sidenav" id="sidenav-mobile">
+            <li><a href="#">Catálogo</a></li>
+            <li><a href="#">Categorías</a></li>
+          </ul>
+        </nav>
+      </header>
+
+
+
+      {{-- Scripts --}}
+      {{-- CDN --}}
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
+      {{-- Main --}}
+      {{-- <script src="/js/manifest.js" type="text/javascript" charset="utf-8"></script> --}}
+      {{-- <script src="/js/vendor.js" type="text/javascript" charset="utf-8"></script> --}}
+      <script src="/js/app.js" type="text/javascript" charset="utf-8"></script>
     </body>
 </html>
