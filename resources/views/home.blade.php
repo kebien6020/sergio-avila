bbeeffd4efc828aefe85e800d69c9282c3b9bcc8
Kevin Peña <kevin.pena.prog@gmail.com>
2018-10-01 16:10:56 -0500

    Extract layout from home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,132 +1,95 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        {{-- CSRF Token --}}
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('name', 'home')
 
-        <title>Sergio Avila</title>
+@section('content')
 
-        {{-- Fonts --}}
-        {{-- Google Icon Font --}}
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<section class="about">
+  <div class="parallax-container">
+    <div class="parallax"><img src="/images/about-bg.jpg"></div>
+    <div class="about-text">
+      <h3>Sobre nosotros</h3>
+      <h5>Texto sobre nosotros</h5>
+    </div>
+  </div>
+</section>
 
-        {{-- Styles --}}
-        {{-- Main styles --}}
-        <link type="text/css" rel="stylesheet" href="css/vendor.css"  media="screen,projection"/>
-        <link type="text/css" rel="stylesheet" href="css/app.css"  media="screen,projection"/>
-
-    </head>
-    <body>
-      <header>
-        @include('partials/navbar')
-      </header>
-
-      <main>
-        <section class="about">
-          <div class="parallax-container">
-            <div class="parallax"><img src="/images/about-bg.jpg"></div>
-            <div class="about-text">
-              <h3>Sobre nosotros</h3>
-              <h5>Texto sobre nosotros</h5>
+<section id="categories">
+  <h3>Categorías</h3>
+  <?php
+    $categs = [
+      ['Agendas', 'agendas'],
+      ['Bebidas', 'bebidas', [
+        ['Botilitos', 'botilitos'],
+        ['Mugs, Tazas y Vasos', 'mugs'],
+      ]],
+      ['Bienestar', 'bienestar', [
+        ['Anti-stress', 'anti-stress'],
+        ['Belleza', 'belleza'],
+        ['Salud', 'salud'],
+      ]],
+      ['Herramientas', 'herramientas'],
+      ['Hogar', 'hogar', [
+        ['Bar', 'bar'],
+        ['Hogar', 'hogar'],
+        ['Relojes y Portarretratos', 'relojes'],
+      ]],
+      ['Niños', 'ninos'],
+      ['Oficina', 'oficina', [
+        ['Boligrafos', 'boligrafos'],
+        ['Libretas y Carpetas', 'libretas'],
+        ['Llaveros', 'llaveros'],
+        ['Oficina', 'oficina'],
+      ]],
+      ['Tecnología', 'tecnologia', [
+        ['Accesorios de cómputo y USB', 'acc-computo'],
+        ['Accesorios de smartphone y tablet', 'acc-cel'],
+        ['Audio y bocinas', 'audio']
+      ]],
+      ['Textiles', 'textiles', [
+        ['Bolsas', 'bolsas'],
+        ['Gorras', 'gorras'],
+        ['Hieleras y loncheras', 'loncheras'],
+        ['Maletas', 'maletas'],
+        ['Mochilas', 'mochilas'],
+        ['Paraguas', 'paraguas'],
+        ['Portafolios', 'portafolios'],
+      ]],
+      ['Viaje', 'viaje'],
+    ]
+  ?>
+  <div class="row">
+    @foreach ($categs as $categ)
+      <a href="#">
+        <div class="col s6 m4 l3">
+          <div class="card hoverable card-outer">
+            <div class="card-image">
+              <img src="images/categ-{{$categ[1]}}.jpg">
             </div>
-          </div>
-        </section>
-        <section id="categories">
-          <h3>Categorías</h3>
-          <?php
-            $categs = [
-              ['Agendas', 'agendas'],
-              ['Bebidas', 'bebidas', [
-                ['Botilitos', 'botilitos'],
-                ['Mugs, Tazas y Vasos', 'mugs'],
-              ]],
-              ['Bienestar', 'bienestar', [
-                ['Anti-stress', 'anti-stress'],
-                ['Belleza', 'belleza'],
-                ['Salud', 'salud'],
-              ]],
-              ['Herramientas', 'herramientas'],
-              ['Hogar', 'hogar', [
-                ['Bar', 'bar'],
-                ['Hogar', 'hogar'],
-                ['Relojes y Portarretratos', 'relojes'],
-              ]],
-              ['Niños', 'ninos'],
-              ['Oficina', 'oficina', [
-                ['Boligrafos', 'boligrafos'],
-                ['Libretas y Carpetas', 'libretas'],
-                ['Llaveros', 'llaveros'],
-                ['Oficina', 'oficina'],
-              ]],
-              ['Tecnología', 'tecnologia', [
-                ['Accesorios de cómputo y USB', 'acc-computo'],
-                ['Accesorios de smartphone y tablet', 'acc-cel'],
-                ['Audio y bocinas', 'audio']
-              ]],
-              ['Textiles', 'textiles', [
-                ['Bolsas', 'bolsas'],
-                ['Gorras', 'gorras'],
-                ['Hieleras y loncheras', 'loncheras'],
-                ['Maletas', 'maletas'],
-                ['Mochilas', 'mochilas'],
-                ['Paraguas', 'paraguas'],
-                ['Portafolios', 'portafolios'],
-              ]],
-              ['Viaje', 'viaje'],
-            ]
-          ?>
-          <div class="row">
-            @foreach ($categs as $categ)
-              <a href="#">
-                <div class="col s6 m4 l3">
-                  <div class="card hoverable card-outer">
-                    <div class="card-image">
-                      <img src="images/categ-{{$categ[1]}}.jpg">
-                    </div>
-                    <div class="card-content">
-                      <span class="card-title">{{$categ[0]}}</span>
-                    </div>
-                    @if (array_key_exists(2, $categ))
-                      <div class="subcategory-container">
-                          @foreach ($categ[2] as $subcateg)
-                            <div class="inner-card-wrapper">
-                              <div class="card hoverable">
-                                <div class="card-image">
-                                  <img src="images/categ-{{$subcateg[1]}}.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title">{{$subcateg[0]}}</span>
-                                </div>
-                              </div>
-                            </div>
-                          @endforeach
+            <div class="card-content">
+              <span class="card-title">{{$categ[0]}}</span>
+            </div>
+            @if (array_key_exists(2, $categ))
+              <div class="subcategory-container">
+                  @foreach ($categ[2] as $subcateg)
+                    <div class="inner-card-wrapper">
+                      <div class="card hoverable">
+                        <div class="card-image">
+                          <img src="images/categ-{{$subcateg[1]}}.jpg">
+                        </div>
+                        <div class="card-content">
+                          <span class="card-title">{{$subcateg[0]}}</span>
+                        </div>
                       </div>
-                    @endif
-                  </div>
-                </div>
-              </a>
-            @endforeach
+                    </div>
+                  @endforeach
+              </div>
+            @endif
           </div>
-        </section>
-      </main>
+        </div>
+      </a>
+    @endforeach
+  </div>
+</section>
 
-
-      {{-- Utility tags --}}
-      <div id='scrim'></div>
-
-      {{-- Scripts --}}
-      {{-- CDN --}}
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
-      {{-- Main --}}
-      {{-- <script src="/js/manifest.js" type="text/javascript" charset="utf-8"></script> --}}
-      {{-- <script src="/js/vendor.js" type="text/javascript" charset="utf-8"></script> --}}
-      <script src="/js/app.js" type="text/javascript" charset="utf-8"></script>
-
-      {{-- View specific js --}}
-      <script src="/js/home.js" type="text/javascript" charset="utf-8"></script>
-    </body>
-</html>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        {{-- CSRF Token --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Sergio Avila</title>
+
+        {{-- Fonts --}}
+        {{-- Google Icon Font --}}
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        {{-- Styles --}}
+        {{-- Main styles --}}
+        <link type="text/css" rel="stylesheet" href="css/vendor.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/app.css"  media="screen,projection"/>
+
+    </head>
+    <body>
+      <header>
+        @include('partials/navbar')
+      </header>
+
+      <main>
+        @yield('content')
+      </main>
+
+
+      {{-- Utility tags --}}
+      <div id='scrim'></div>
+
+      {{-- Scripts --}}
+      {{-- CDN --}}
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
+      {{-- Main --}}
+      {{-- <script src="/js/manifest.js" type="text/javascript" charset="utf-8"></script> --}}
+      {{-- <script src="/js/vendor.js" type="text/javascript" charset="utf-8"></script> --}}
+      <script src="/js/app.js" type="text/javascript" charset="utf-8"></script>
+
+      {{-- View specific js --}}
+      <script src="/js/@yield('name').js" type="text/javascript" charset="utf-8"></script>
+    </body>
+</html>
