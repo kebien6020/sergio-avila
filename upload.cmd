f6fd81913b5fcf8d99f3e61435f7c758534ac77a
Kevin Peña <kevin.pena.prog@gmail.com>
2018-10-03 11:08:01 -0500

    Add upload script

---
+++ upload.cmd
@@ -0,0 +1,24 @@
+rsync -avz -e ssh ^
+--chmod=755 ^
+--include="/app/***" ^
+--include="/bootstrap/***" ^
+--include="/config/***" ^
+--include="/database/***" ^
+--include="/public/***" ^
+--include="/resources/***" ^
+--include="/routes/***" ^
+--include="/storage/***" ^
+--include="/tests/***" ^
+--include="/.env.example" ^
+--include="/artisan" ^
+--include="/composer.json" ^
+--include="/composer.lock" ^
+--include="/dump.sql" ^
+--include="/package-lock.json" ^
+--include="/package.json" ^
+--include="/phpunit.xml" ^
+--include="/server.php" ^
+--include="/webpack.mix.js" ^
+--include="/yarn.lock" ^
+--exclude="*" ^
+. kevin@kevinpena.com:/var/www/sergio-avila/
