781c62da953c92ce33ad109267156b8b4addd26f
Kevin Peña <kevin.pena.prog@gmail.com>
2018-10-03 16:59:20 -0500

    Resize image slider responsively

--- public/js/item.js
+++ public/js/item.js
@@ -83,79 +83,80 @@ module.exports = __webpack_require__(14);
 /***/ 14:
 /***/ (function(module, exports, __webpack_require__) {
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var M = __webpack_require__(0);
 
-var sliderElem = document.querySelectorAll('.slider');
-var sliders = M.Slider.init(sliderElem, {
-  indicators: false,
-  duration: 300
-});
+function sliderResponsiveHeight() {
+  var res = 350;
+  if (window.innerWidth > 450) res = 450;
+  if (window.innerWidth > 600) res = 500;
+  if (window.innerWidth > 800) res = 600;
+  if (window.innerWidth > 992) res = 350;
 
-var _iteratorNormalCompletion = true;
-var _didIteratorError = false;
-var _iteratorError = undefined;
-
-try {
-  for (var _iterator = sliders[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    var slider = _step.value;
-
-    slider.pause();
-  }
-} catch (err) {
-  _didIteratorError = true;
-  _iteratorError = err;
-} finally {
-  try {
-    if (!_iteratorNormalCompletion && _iterator.return) {
-      _iterator.return();
-    }
-  } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
-    }
-  }
+  return res;
 }
 
-var thumbnails = document.querySelectorAll('.thumb-img');
+var sliderElem = document.querySelectorAll('.slider');
+var sliderOpts = {
+  indicators: false,
+  duration: 300,
+  height: sliderResponsiveHeight()
+};
+var sliders = M.Slider.init(sliderElem, sliderOpts);
 
-var _loop = function _loop($thumb) {
-  $thumb.addEventListener('mouseenter', function () {
-    var _iteratorNormalCompletion3 = true;
-    var _didIteratorError3 = false;
-    var _iteratorError3 = undefined;
+// Resize slider resposively on resize, debouncing with requestAnimationFrame
+
+var timeout = void 0;
+window.addEventListener('resize', function (event) {
+  // If there was a previous timer, cancel it
+  if (timeout) window.cancelAnimationFrame(timeout);
+
+  // Schedule slider resize on the next animation frame
+  timeout = window.requestAnimationFrame(function () {
+    var height = sliderResponsiveHeight();
+
+    // If we are not going to change anything abort
+    if (height === sliders[0].options.height) return;
+
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
     try {
-      for (var _iterator3 = sliders[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-        var _slider = _step3.value;
+      for (var _iterator = sliders[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var slider = _step.value;
 
-        _slider.set($thumb.dataset.num);
+        slider.destroy();
       }
     } catch (err) {
-      _didIteratorError3 = true;
-      _iteratorError3 = err;
+      _didIteratorError = true;
+      _iteratorError = err;
     } finally {
       try {
-        if (!_iteratorNormalCompletion3 && _iterator3.return) {
-          _iterator3.return();
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
         }
       } finally {
-        if (_didIteratorError3) {
-          throw _iteratorError3;
+        if (_didIteratorError) {
+          throw _iteratorError;
         }
       }
     }
+
+    sliders = M.Slider.init(sliderElem, _extends({}, sliderOpts, { height: height }));
   });
-};
+}, false);
 
 var _iteratorNormalCompletion2 = true;
 var _didIteratorError2 = false;
 var _iteratorError2 = undefined;
 
 try {
-  for (var _iterator2 = thumbnails[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-    var $thumb = _step2.value;
+  for (var _iterator2 = sliders[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+    var slider = _step2.value;
 
-    _loop($thumb);
+    slider.pause();
   }
 } catch (err) {
   _didIteratorError2 = true;
@@ -168,6 +169,62 @@ try {
   } finally {
     if (_didIteratorError2) {
       throw _iteratorError2;
+    }
+  }
+}
+
+var thumbnails = document.querySelectorAll('.thumb-img');
+
+var _loop = function _loop($thumb) {
+  $thumb.addEventListener('mouseenter', function () {
+    var _iteratorNormalCompletion4 = true;
+    var _didIteratorError4 = false;
+    var _iteratorError4 = undefined;
+
+    try {
+      for (var _iterator4 = sliders[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+        var _slider = _step4.value;
+
+        _slider.set($thumb.dataset.num);
+      }
+    } catch (err) {
+      _didIteratorError4 = true;
+      _iteratorError4 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion4 && _iterator4.return) {
+          _iterator4.return();
+        }
+      } finally {
+        if (_didIteratorError4) {
+          throw _iteratorError4;
+        }
+      }
+    }
+  });
+};
+
+var _iteratorNormalCompletion3 = true;
+var _didIteratorError3 = false;
+var _iteratorError3 = undefined;
+
+try {
+  for (var _iterator3 = thumbnails[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+    var $thumb = _step3.value;
+
+    _loop($thumb);
+  }
+} catch (err) {
+  _didIteratorError3 = true;
+  _iteratorError3 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion3 && _iterator3.return) {
+      _iterator3.return();
+    }
+  } finally {
+    if (_didIteratorError3) {
+      throw _iteratorError3;
     }
   }
 }
--- resources/js/item.js
+++ resources/js/item.js
@@ -1,10 +1,46 @@
 const M = require('materialize-css')
 
+function sliderResponsiveHeight() {
+  let res = 350
+  if (window.innerWidth > 450) res = 450
+  if (window.innerWidth > 600) res = 500
+  if (window.innerWidth > 800) res = 600
+  if (window.innerWidth > 992) res = 350
+
+  return res
+}
+
 const sliderElem = document.querySelectorAll('.slider')
-const sliders = M.Slider.init(sliderElem, {
+const sliderOpts = {
   indicators: false,
   duration: 300,
-})
+  height: sliderResponsiveHeight(),
+}
+let sliders = M.Slider.init(sliderElem, sliderOpts)
+
+// Resize slider resposively on resize, debouncing with requestAnimationFrame
+
+let timeout
+window.addEventListener('resize', function ( event ) {
+	// If there was a previous timer, cancel it
+	if (timeout)
+		window.cancelAnimationFrame(timeout)
+
+  // Schedule slider resize on the next animation frame
+	timeout = window.requestAnimationFrame(() => {
+    const height = sliderResponsiveHeight()
+
+    // If we are not going to change anything abort
+    if (height === sliders[0].options.height)
+      return
+
+    for (const slider of sliders)
+      slider.destroy()
+    sliders = M.Slider.init(sliderElem, {...sliderOpts, height})
+
+	})
+
+}, false);
 
 for (const slider of sliders)
   slider.pause()
