87044378d81c483cf61d84d2925d697b141bcadc
Kevin Peña <kevin.pena.prog@gmail.com>
2018-10-05 16:15:10 -0500

    Add contact info placeholder to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,8 +2,29 @@
   <div class="container">
     <div class="row">
       <div class="col l4 s12">
-        <h5 class="white-text">Pongase en contacto</h5>
+        <h5 class="white-text">Entra en contacto</h5>
         <p class="grey-text text-lighten-4">La información de contacto se ubica aquí.</p>
+        <div class="sub-contact">
+          <div class="sub-icon">
+            <i class="material-icons">location_on</i>
+          </div>
+          <div class="sub-text">
+            <h6>Nuestra Oficina</h6>
+            <p>Calle x # zz - zz</p>
+            <p>Bogotá D.C.</p>
+          </div>
+        </div>
+        <div class="sub-contact">
+          <div class="sub-icon">
+            <i class="material-icons">phone_iphone</i>
+          </div>
+          <div class="sub-text">
+            <h6>Llamanos</h6>
+            <p>Tel: xxx xxxx</p>
+            <p>Cel: xxx xxx xxxx</p>
+          </div>
+        </div>
+
       </div>
       <div class="col l6 offset-l2 s12">
         <h5 class="white-text">Encuentranos</h5>
