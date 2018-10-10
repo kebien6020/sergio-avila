5c88f1adf3dc2b2720ed9460053bdf61fa277b2e
Kevin Peña <kevin.pena.prog@gmail.com>
2018-10-10 17:06:48 -0500

    Make Item searchable

--- app/Item.php
+++ app/Item.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Item extends Model
 {
+    use Searchable;
+
+    public $asYouType = true;
+
     public function family() {
       return $this->belongsTo('App\Family', 'family_code', 'code');
     }
