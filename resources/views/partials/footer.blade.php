1b9bf25961167711881d5d0301f3c6692b2a9a36
Kevin Peña <kevin.pena.prog@gmail.com>
2018-10-19 20:16:17 -0500

    Misc updates to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,63 +1,61 @@
 <footer class="page-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col l4 s12">
-        <h5 class="white-text">Entra en contacto</h5>
-        <p class="grey-text text-lighten-4">Nos puedes encontrar en los siguientes medios.</p>
-        <div class="sub-contact">
-          <div class="sub-icon">
-            <i class="material-icons">location_on</i>
-          </div>
-          <div class="sub-text">
-            <h6>Oficina</h6>
-            <p>Carrera 14a 19 - 91 Sur</p>
-            <p>Bogotá D.C.</p>
-          </div>
+  <div class="row">
+    <div class="col l6 s12 footer-contact">
+      <h5 class="white-text">Entra en contacto</h5>
+      <p class="grey-text text-lighten-4">Nos puedes encontrar en los siguientes medios.</p>
+      <div class="sub-contact">
+        <div class="sub-icon">
+          <i class="material-icons">location_on</i>
         </div>
-        <div class="sub-contact">
-          <div class="sub-icon">
-            <i class="material-icons">location_on</i>
-          </div>
-          <div class="sub-text">
-            <h6>Planta de Producción</h6>
-            <p>Calle 22d Sur 4 - 04</p>
-            <p>Bogotá D.C.</p>
-          </div>
+        <div class="sub-text">
+          <h6>Oficina</h6>
+          <p>Carrera 14a 19 - 91 Sur</p>
+          <p>Bogotá D.C.</p>
         </div>
-        <div class="sub-contact">
-          <div class="sub-icon">
-            <i class="material-icons">phone_iphone</i>
-          </div>
-          <div class="sub-text">
-            <h6>Llamanos</h6>
-            <p>PBX.: 1 + 361 50 61</p>
-            <p>Cel: 305 717 32 18 - 304 364 43 24</p>
-          </div>
+      </div>
+      <div class="sub-contact">
+        <div class="sub-icon">
+          <i class="material-icons">location_on</i>
         </div>
-        <div class="sub-contact">
-          <div class="sub-icon">
-            <i class="material-icons">email</i>
-          </div>
-          <div class="sub-text">
-            <h6>Correo Electronico</h6>
-            <p>manager@texto-print.com</p>
-          </div>
+        <div class="sub-text">
+          <h6>Planta de Producción</h6>
+          <p>Calle 22d Sur 4 - 04</p>
+          <p>Bogotá D.C.</p>
         </div>
-        <div class="sub-contact">
-          <div class="sub-icon">
-            <i class="material-icons">web</i>
-          </div>
-          <div class="sub-text">
-            <h6>Otro Sitio del Grupo</h6>
-            <img src="/images/logo-alt-site.png" alt="Logo otro sitio">
-          </div>
+      </div>
+      <div class="sub-contact">
+        <div class="sub-icon">
+          <i class="material-icons">phone_iphone</i>
         </div>
+        <div class="sub-text">
+          <h6>Llamanos</h6>
+          <p>PBX.: 1 + 361 50 61</p>
+          <p>Cel: 305 717 32 18 - 304 364 43 24</p>
+        </div>
+      </div>
+      <div class="sub-contact">
+        <div class="sub-icon">
+          <i class="material-icons">email</i>
+        </div>
+        <div class="sub-text">
+          <h6>Correo Electronico</h6>
+          <p>manager@texto-print.com</p>
+        </div>
+      </div>
+      <div class="sub-contact">
+        <div class="sub-icon">
+          <i class="material-icons">web</i>
+        </div>
+        <div class="sub-text">
+          <h6>Otro Sitio del Grupo</h6>
+          <img src="/images/logo-alt-site.png" alt="Logo otro sitio">
+        </div>
+      </div>
 
-      </div>
-      <div class="col l6 offset-l2 s12">
-        <h5 class="white-text">Encuentranos</h5>
-        <iframe class="map" frameborder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.071727675662!2d-74.10300818570646!3d4.581142643959356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f98dd8b770b6b%3A0xcbf189b260f745dc!2sTexto-print!5e0!3m2!1ses-419!2sco!4v1526045668428" allowfullscreen></iframe>
-      </div>
+    </div>
+    <div class="col l6 s12 footer-map">
+      <h5 class="white-text">Encuentranos</h5>
+      <iframe class="map" frameborder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.071727675662!2d-74.10300818570646!3d4.581142643959356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f98dd8b770b6b%3A0xcbf189b260f745dc!2sTexto-print!5e0!3m2!1ses-419!2sco!4v1526045668428" allowfullscreen></iframe>
     </div>
   </div>
   <div class="footer-copyright">
